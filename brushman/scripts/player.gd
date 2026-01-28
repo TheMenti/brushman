ebaa7504c0e736b0f015882c9eeaaf0c3c701f68
Antonio Volpe <antoniovolpe785@gmail.com>
2026-01-28 10:33:27 +0100

    Movement completion

--- brushman/scripts/player.gd
+++ brushman/scripts/player.gd
@@ -1,26 +1,20 @@
 extends CharacterBody2D
 @onready var _animated_sprite = $player_animated_sprites
+var Health:float = 5.0 
 
+#const Movement = preload("movemente.gd")
 const SPEED = 300.0
 const JUMP_VELOCITY = -300.0
 
 func play_anim(name: String) -> void:
 	if _animated_sprite.animation != name:
 		_animated_sprite.play(name)
-		
-func falling_state(vel) -> bool:
-		if vel > 0:
-			return true
-		else:
-			return false
 
 func _physics_process(delta: float) -> void:
 	
 	# Gravità
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		#if falling_state(velocity.y):
-			#play_anim("falling")
 		
 		
 
@@ -28,14 +22,14 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		play_anim("jumping")
-		if falling_state(velocity.y):
-			play_anim("falling")
+
 	
 	elif Input.is_action_pressed("jump") and not is_on_floor():
 		velocity.y += delta * JUMP_VELOCITY - 3
-		play_anim("jumping")
-		if falling_state(velocity.y):
+		if velocity.y > 0:
 			play_anim("falling")
+		else:
+			play_anim("jumping")
 
 	# Movimento orizzontale
 	var direction := Input.get_axis("move_left", "move_right")
@@ -52,7 +46,11 @@ func _physics_process(delta: float) -> void:
 
 	# Animazione in aria
 	if not is_on_floor():
-		play_anim("jumping")
+		if velocity.y > 0:
+			play_anim("falling")
+		else:
+			play_anim("jumping")
+		
 
 	move_and_slide()
 
