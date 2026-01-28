7acbe905fd8202ca0267b01bf79cf1333e5f1a75
Antonio Volpe <antoniovolpe785@gmail.com>
2026-01-28 11:04:56 +0100

    adjusted collision

--- brushman/scripts/hitbox.gd
+++ brushman/scripts/hitbox.gd
@@ -13,18 +13,18 @@ func _ready() -> void:
 	area_entered.connect(_area_collided)
 	
 	if hitbox_liftime > 0.0:
-		var mew_timer = Timer.new()  #inizializza nuovo timer
+		var new_timer = Timer.new()  #inizializza nuovo timer
 		add_child(new_timer)  #aggiunge child nodo timer
 		new_timer.timeout.connect(queue_free)  #eliminazione del timer quando finito
 		new_timer.call_deferred("start", hitbox_liftime)
 	
-	if shape:
-		var collision_shape = CollisionShape2d.new()
-		collision_shape.shape = shape
+	if hitbox_shape:
+		var collision_shape = CollisionShape2D.new()
+		collision_shape.shape = hitbox_shape
 		add_child(collision_shape)
 	
 	set_collision_layer_value(1, false)
 	set_collision_mask_value(1, false)
 	
-func _area_collided(area: Area2D) -> void 
+func _area_collided(area: Area2D) -> void:
 	pass
