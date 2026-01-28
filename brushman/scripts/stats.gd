33ef9592e8974eef8bce04b836f994cd90ca2b2a
Antonio Volpe <antoniovolpe785@gmail.com>
2026-01-28 13:23:36 +0100

    colli di cazzo

--- brushman/scripts/stats.gd
+++ brushman/scripts/stats.gd
@@ -4,7 +4,22 @@ enum faction{
 	PLAYER,
 	ENEMY,
 }
-@export var health:float 
+@export var max_health:float = 100.0
 @export var damage:float
 signal health_reduced
 signal death 
+
+var health: int = 0: set = _on_health_set
+
+func _init() -> void:
+	initialize_resources.call_deferred()
+	
+func initialize_resources() -> void:
+	health = max_health
+	print(health)
+	
+func _on_health_set(new_value: int) -> void:
+	health = new_value
+	if health <= 0:
+		death.emit()
+		
