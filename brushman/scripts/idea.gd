85c4443d6b6d535eddf41907e63c556c5a1ad6fb
gd <dare.gdevil09@gmail.com>
2026-01-29 19:13:09 +0100

    Idea sprites, collision and script

--- brushman/scripts/idea.gd
+++ brushman/scripts/idea.gd
@@ -1,16 +1,27 @@
 extends Area2D
 
+@onready var animation = $AnimatedSprite2D
+@onready var collision = $CollisionShape2D
+var touched = false
+signal picked
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if touched:
+		animation.play("picked")
+		animation.animation_finished.connect(_on_destroy)
+	else:
+		animation.play("default")
 
+func _on_destroy():
+	queue_free()
 
 func _on_area_entered(area: Area2D) -> void:
-	queue_free()
-	pass # Replace with function body.
+	touched = true
+	picked.emit()
+	set_deferred("monitoring", false)
+	set_deferred("monitorable", false)
