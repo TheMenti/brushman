abc1307cc8d060809a4e16d9b961ce989a7fb174
Antonio Volpe <antoniovolpe785@gmail.com>
2026-01-30 19:05:16 +0100

    changes on enemy hitboxes

--- brushman/scripts/enemies/rubber.gd
+++ brushman/scripts/enemies/rubber.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @export var stats:Stats
 const SPEED = 30.0
 const JUMP_VELOCITY = -30.0
+var direction:int
+@onready var sprite: Sprite2D = $Sprite2D
 
 func _ready():
 	$VisibleOnScreenNotifier2D.screen_exited.connect(_on_screen_exited)
@@ -10,10 +12,8 @@ func _ready():
 func _on_screen_exited():
 	queue_free()
 
-func _on_area_entered(body):
-	print("body_entered rubber")
-	if body.get_parent().get_node("Player").stats.Faction.PLAYER:
-		queue_free()
+func _on_area_2d_area_entered(area: Area2D):
+	queue_free()
 
 func _process(delta):
 	# Move from right to left
@@ -24,8 +24,4 @@ func _process(delta):
 func _physics_process(delta: float) -> void:
 	velocity.x = JUMP_VELOCITY
 	move_and_slide()
-
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.get_parent().get_node("Player").stats.Faction.PLAYER:
-		queue_free()
+	
