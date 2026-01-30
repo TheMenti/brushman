c6d4f229af67fb42cb03d928e97ff4b063b8f556
gd <dare.gdevil09@gmail.com>
2026-01-30 19:37:37 +0100

    ideas

--- brushman/scripts/idea.gd
+++ brushman/scripts/idea.gd
@@ -1,16 +1,27 @@
-extends Area2D
+class_name Idea extends Area2D
 
+@onready var animation = $AnimatedSprite2D
+@onready var collision = $CollisionShape2D
+var touched = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if touched:
+		animation.play("picked")
+	else:
+		animation.play("default")
 
+func _on_destroy():
+	queue_free()
 
 func _on_area_entered(area: Area2D) -> void:
-	queue_free()
-	pass # Replace with function body.
+	print("LOG - [picked idea]")
+	touched = true
+	get_tree().call_group("UI", "update_ideas_counter")
+	set_deferred("monitoring", false)
+	set_deferred("monitorable", false)
+	animation.animation_finished.connect(_on_destroy)
