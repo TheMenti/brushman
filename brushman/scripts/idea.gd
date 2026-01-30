d2427cd58e13ecbffd91a2621c95f51a2f57550b
gd <dare.gdevil09@gmail.com>
2026-01-30 03:52:55 +0100

    idea counter

--- brushman/scripts/idea.gd
+++ brushman/scripts/idea.gd
@@ -1,9 +1,8 @@
-extends Area2D
+class_name Idea extends Area2D
 
 @onready var animation = $AnimatedSprite2D
 @onready var collision = $CollisionShape2D
 var touched = false
-signal picked
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,7 +21,7 @@ func _on_destroy():
 func _on_area_entered(area: Area2D) -> void:
 	print("LOG - [picked idea]")
 	touched = true
-	picked.emit()
+	get_tree().call_group("UI", "update_ideas_counter")
 	set_deferred("monitoring", false)
 	set_deferred("monitorable", false)
 	animation.animation_finished.connect(_on_destroy)
