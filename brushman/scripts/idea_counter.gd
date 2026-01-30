fdf12e81fe837573b04d33793e12befaec56e210
gd <dare.gdevil09@gmail.com>
2026-01-30 19:37:37 +0100

    ideas

--- brushman/scripts/idea_counter.gd
+++ brushman/scripts/idea_counter.gd
@@ -1,17 +1,16 @@
-extends CanvasLayer
+class_name IdeaCounter extends Node
 
-@onready var health_bar_sprite = $AnimatedSprite2D
 @onready var level_ideas_label = $Label
 var ideas_collected = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	health_bar_sprite.pause()
-	health_bar_sprite.frame = 5
+	pass # Replace with function body.
 
-func update_sprite(hp_player):
-	health_bar_sprite.frame = hp_player
-	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
 func update_ideas_counter():
 	ideas_collected += 1
 	level_ideas_label.set_deferred("text", str(ideas_collected))
