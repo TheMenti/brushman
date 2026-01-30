ce9e286e58377c1cea592ac39a505d05f525bab2
Antonio Volpe <antoniovolpe785@gmail.com>
2026-01-30 19:49:32 +0100

    Completed enemys, hitboxes fix

--- brushman/scripts/enemies/rubber.gd
+++ brushman/scripts/enemies/rubber.gd
@@ -6,22 +6,17 @@ const JUMP_VELOCITY = -30.0
 var direction:int
 @onready var sprite: Sprite2D = $Sprite2D
 
-func _ready():
-	$VisibleOnScreenNotifier2D.screen_exited.connect(_on_screen_exited)
-
-func _on_screen_exited():
-	queue_free()
 
 func _on_area_2d_area_entered(area: Area2D):
 	queue_free()
 
-func _process(delta):
+#func _process(delta):
 	# Move from right to left
-	position.x -= SPEED * delta
-	velocity.x -= -1 * SPEED
+	#position.x -= SPEED * delta
+	#velocity.x -= -1 * SPEED
 
 
-func _physics_process(delta: float) -> void:
-	velocity.x = JUMP_VELOCITY
-	move_and_slide()
-	
+#func _physics_process(delta: float) -> void:
+	#velocity.x = JUMP_VELOCITY
+	#move_and_slide()
+	#
